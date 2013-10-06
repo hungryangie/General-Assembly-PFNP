fcc6b6d5e6f38ea63423da059f89d911f5b2f411
Angelica Hom <angelica.hom@gmail.com>
2013-10-06 15:19:40 -0700

    Created moneybags app

---
+++ moneybags/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moneybags::Application.config.secret_key_base = 'becddf89c100d4a4e9fcd9b2e6f1128eefe29bffdb193c5b02bf981c7ef5269c38f9dbfc30d51dca190222ffd3a3fd6ea4a1239824df01378096b2749da1cd86'
